681befa1cfb0d583714239b0b93a3b4b520c7439
elifxa <elif.akcay@std.antalya.edu.tr>
2023-08-07 14:06:46 +0300

    Initial commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,30 @@
+import { Input, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModalComponent } from './modal/modal.component';
+import { ModalService } from '../services/modal.service';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
+import { SharedComponent } from './shared.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { provideEnvironmentNgxMask, NgxMaskDirective } from 'ngx-mask';
+
+@NgModule({
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    SharedComponent,
+    InputComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule, NgxMaskDirective],
+  exports: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+  ],
+  providers: [provideEnvironmentNgxMask()],
+  //providers: [ModalService],
+})
+export class SharedModule {}
